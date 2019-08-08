4492066999c42e05a1ce5ce5e36a073c95d6ae98
pstadler1990 <pstadler90@gmx.de>
2019-08-08 23:17:18 +0200

    added meta loading

--- log/log.py
+++ log/log.py
@@ -4,9 +4,20 @@ import json
 import uuid
 import yaml
 from datetime import datetime
-from exceptions import LogNoLoggableEntryError, LogEntryAlreadyInLogError
+from termcolor import colored
+from helpers import file_get_extension
+from exceptions import *
+from loader.loaders import find_meta_loader_for_ext
 
 config = yaml.safe_load(open('config.yml'))
+
+
+def file_is_meta(ext):
+    return ext in config['files']['meta_types']
+
+
+def file_is_page(ext):
+    return ext in config['files']['page_types']
 
 
 class Log:
@@ -23,6 +34,50 @@ class Log:
         except json.decoder.JSONDecodeError:
             self.entries = []
 
+    @staticmethod
+    def load_raw_entries(path):
+        """
+        Load a given directory containing meta (json) and page data (md)
+        Returns a list of all found entries
+        """
+        if not os.path.isdir(path):
+            raise LogEntriesNotADirectoryError('Given path is not a directory')
+
+        found_files = {
+            'meta': [],
+            'pages': []
+        }
+
+        print(colored('Finding meta and page files in...', 'yellow'), path)
+        for file in os.listdir(path):
+            absolute_file_path = os.path.join(path, file)
+
+            if os.path.isfile(absolute_file_path):
+
+                # Finding meta and page files
+                ext = file_get_extension(absolute_file_path, strip_dot=True)
+
+                if file_is_meta(ext):
+                    print('[meta]', colored(file, 'magenta'))
+
+                    # Check available loaders
+                    loader = find_meta_loader_for_ext(ext)()
+                    if not loader:
+                        raise LoaderNoSuitableLoaderError('No suitable loader found for this type')
+
+                    found_files['meta'].append(loader.read(absolute_file_path))
+
+                elif file_is_page(ext):
+                    print('[page]', colored(file, 'blue'))
+
+                    # TODO: Load page contents into dict
+
+                else:
+                    # Skipping unrelated files
+                    pass
+
+        return found_files
+
     def insert(self, entry, auto_meta=True):
         """
         Insert a new loggable object to the log
@@ -38,8 +93,6 @@ class Log:
             entry.last_modified = datetime.now()
             if not entry.version:
                 entry.version = 1
-            else:
-                entry.version += 1
 
         entry.uid = uuid.uuid4()
         self.entries.append(entry)
@@ -56,17 +109,16 @@ class Log:
         """
         try:
             if self.log_file.writable():
-                try:
-                    json.dump([e.serialize() for e in self.entries], self.log_file, ensure_ascii=False)
-                except TypeError:
-                    # TODO: error
-                    pass
+                json.dump([e.serialize() for e in self.entries], self.log_file, ensure_ascii=False)
+
         except ValueError:
-            # TODO: Error
-            pass
+            raise LogNotWriteableError('Log is not writable')
         finally:
             if close:
                 self.close()
 
     def close(self):
+        """
+        Close the log file
+        """
         self.log_file.close()
