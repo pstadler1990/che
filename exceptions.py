a519b487b5e42320e4235afe46e987a9ff13f0eb
pstadler1990 <pstadler90@gmx.de>
2019-08-08 23:17:37 +0200

    added more exceptions for logging and loading

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,23 @@ class LogNoLoggableEntryError(Exception):
 
 class LogEntryAlreadyInLogError(Exception):
     pass
+
+
+class LogNotWriteableError(Exception):
+    pass
+
+
+class LogEntryNotInLogError(Exception):
+    pass
+
+
+class LogEntriesNotADirectoryError(Exception):
+    pass
+
+
+class LoaderWrongFileError(Exception):
+    pass
+
+
+class LoaderNoSuitableLoaderError(Exception):
+    pass
