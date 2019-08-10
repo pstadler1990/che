2ed7b4986a892147681250cdcf303ba924d07e49
pstadler1990 <pstadler90@gmx.de>
2019-08-10 14:15:37 +0200

    updated to new workflow

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import os
 from termcolor import colored
-from log.entry import Entry
 from log.log import Log
 
 if __name__ == '__main__':
 
     log = Log()
 
-    files = log.load_raw_entries(os.path.join('test'))
+    files, ok = log.load_raw_entries(os.path.join('test'))
+    # this would return false for ok if any file is not a pair (= missing either a meta or a page file)
+
+    print('File integrity: ', colored('OK ', 'green') if ok else colored('Error!', 'red'))
+    print(files)
+
+    log.convert_raw_entries(files)
 
     log.write()
