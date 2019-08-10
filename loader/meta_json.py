a9f990535738f4d8712dda6696dd58678b123414
pstadler1990 <pstadler90@gmx.de>
2019-08-10 15:12:34 +0200

    added markdown loader

--- loader/meta_json.py
+++ loader/meta_json.py
@@ -1,8 +1,5 @@
-import io
 import json
-from exceptions import LoaderWrongFileError
 from loader.default import AMetaLoader
-from helpers import file_get_extension
 
 
 class MetaLoaderJSON(AMetaLoader):
@@ -10,10 +7,5 @@ class MetaLoaderJSON(AMetaLoader):
     Concrete implementation of a meta loader for JSON files
     Reads a given json file into a dict
     """
-    def read(self, file):
-        ext, _ = file_get_extension(file, strip_dot=True)
-        if ext not in ['js', 'json']:
-            raise LoaderWrongFileError('Given file is not a valid json file')
-
-        with io.open(file, 'r', encoding='utf-8') as json_file:
-            return json.load(json_file)
+    def read(self, contents):
+        return json.loads(contents)
