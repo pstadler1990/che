a41efdfbaf6a894355a58c63e29520b16ee518dd
pstadler1990 <pstadler90@gmx.de>
2019-08-17 21:30:03 +0200

    renamed file and added time logging

--- che.py
+++ che.py
@@ -1,9 +1,12 @@
 import os
+import time
 from termcolor import colored
 from log.log import Log
 from builder.build import Builder
 
 if __name__ == '__main__':
+
+    build_time_start = time.time()
 
     log = Log()
 
@@ -13,17 +16,21 @@ if __name__ == '__main__':
     # this would return false for ok if any file is not a pair (= missing either a meta or a page file)
     print('File integrity: ', colored('OK ', 'green') if ok else colored('Error!', 'red'))
 
-    print(files)
-
     changed_files, needs_rebuild = log.convert_raw_entries(files)
 
     builder = Builder(changed_files if not needs_rebuild else files)
     builder.prepare()
     builder.process_text_auto()
 
-    builder.build_nav(files, use_absolute_links=False)
+    if changed_files:
+        builder.build_nav(files, use_absolute_links=False)
 
     # Render html to Jinja template
     builder.build()
 
+    # Update log file after the successful build
     log.write()
+
+    print(colored('BUILD SUCCESSFUL', 'green'), 'Build time: {0}'.format(time.time() - build_time_start))
+
+
